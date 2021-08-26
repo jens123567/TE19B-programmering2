3c3dbf41b452be7e0e12ad8f9045107b11f8cfb9
jens123567 <jens.malmqvist03@gmail.com>
2021-08-26 10:23:51 +0200

    Labb 1

---
+++ Labb1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labb1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textRuta.Text = "....";
+
+        }
+        private void Skicka_Click(object sender, RoutedEventArgs e)
+        {
+            // Skriv ut en dialogruta
+            //MessageBox.Show(inputruta.Text);
+
+            string inTexten = "";
+
+            // Läs in från formuläret
+            inTexten = inputRuta.Text;
+
+            // Skriv ut i formuläret
+            textRuta.Text = inTexten;
+        }
+    }
+}
+
